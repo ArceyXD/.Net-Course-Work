285e41ec09941f1f46d8ac98f8d88a0b2c4f352b
Russell <90806285+ArceyXD@users.noreply.github.com>
2022-03-31 23:14:24 -0400

    Add files via upload

---
+++ Lab11/Lab11/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Lab11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startValue = new System.Windows.Forms.TextBox();
+            this.endValue = new System.Windows.Forms.TextBox();
+            this.startLabel = new System.Windows.Forms.Label();
+            this.endLabel = new System.Windows.Forms.Label();
+            this.headerText = new System.Windows.Forms.RichTextBox();
+            this.graphButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startValue
+            // 
+            this.startValue.Location = new System.Drawing.Point(504, 44);
+            this.startValue.Name = "startValue";
+            this.startValue.Size = new System.Drawing.Size(38, 20);
+            this.startValue.TabIndex = 0;
+            this.startValue.KeyDown += new System.Windows.Forms.KeyEventHandler(this.startValue_KeyDown);
+            // 
+            // endValue
+            // 
+            this.endValue.Location = new System.Drawing.Point(612, 44);
+            this.endValue.Name = "endValue";
+            this.endValue.Size = new System.Drawing.Size(39, 20);
+            this.endValue.TabIndex = 1;
+            this.endValue.KeyDown += new System.Windows.Forms.KeyEventHandler(this.startValue_KeyDown);
+            // 
+            // startLabel
+            // 
+            this.startLabel.AutoSize = true;
+            this.startLabel.Location = new System.Drawing.Point(466, 47);
+            this.startLabel.Name = "startLabel";
+            this.startLabel.Size = new System.Drawing.Size(32, 13);
+            this.startLabel.TabIndex = 2;
+            this.startLabel.Text = "Start:";
+            // 
+            // endLabel
+            // 
+            this.endLabel.AutoSize = true;
+            this.endLabel.Location = new System.Drawing.Point(577, 47);
+            this.endLabel.Name = "endLabel";
+            this.endLabel.Size = new System.Drawing.Size(29, 13);
+            this.endLabel.TabIndex = 3;
+            this.endLabel.Text = "End:";
+            // 
+            // headerText
+            // 
+            this.headerText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.headerText.Location = new System.Drawing.Point(364, 12);
+            this.headerText.Name = "headerText";
+            this.headerText.ReadOnly = true;
+            this.headerText.Size = new System.Drawing.Size(477, 26);
+            this.headerText.TabIndex = 4;
+            this.headerText.Text = "Enter the Start and End value to plot the SINE function";
+            
+            // 
+            // graphButton
+            // 
+            this.graphButton.Location = new System.Drawing.Point(690, 42);
+            this.graphButton.Name = "graphButton";
+            this.graphButton.Size = new System.Drawing.Size(75, 23);
+            this.graphButton.TabIndex = 5;
+            this.graphButton.Text = "Graph";
+            this.graphButton.UseVisualStyleBackColor = true;
+            this.graphButton.Click += new System.EventHandler(this.graphButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1155, 843);
+            this.Controls.Add(this.graphButton);
+            this.Controls.Add(this.headerText);
+            this.Controls.Add(this.endLabel);
+            this.Controls.Add(this.startLabel);
+            this.Controls.Add(this.endValue);
+            this.Controls.Add(this.startValue);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox startValue;
+        private System.Windows.Forms.TextBox endValue;
+        private System.Windows.Forms.Label startLabel;
+        private System.Windows.Forms.Label endLabel;
+        private System.Windows.Forms.RichTextBox headerText;
+        private System.Windows.Forms.Button graphButton;
+    }
+}
---
+++ Lab11/Lab11/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab11
+{
+    public partial class Form1 : Form
+    {
+
+        double min, max;
+        Rectangle r;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void paint()
+        {
+            if (startValue.Text == "" || endValue.Text == "")
+            {
+                MessageBox.Show("Missing Parameters! Try again.");
+                return;
+            }
+            else if (Double.TryParse(startValue.Text, out min) && Double.TryParse(endValue.Text, out max))
+            {
+                if (min > max)
+                {
+                    MessageBox.Show("Start Value cannot be greater than end value.");
+                    return;
+                }
+                Invalidate();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Parameters! Try again.");
+                return;
+            }
+        }
+
+        private void graphButton_Click(object sender, EventArgs e)
+        {
+            paint();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+           
+            Pen blackPen = new Pen(Color.Black, 3);
+
+            //Area to draw graph
+            r = new Rectangle(20, 100, this.ClientSize.Width - 50, this.ClientSize.Height - 150);
+
+        
+            Graphics g = e.Graphics;
+            g.DrawRectangle(blackPen, r);
+            Size size = r.Size;    //Size of the drawing area
+
+            double[] x = new double[size.Width];
+            double[] y = new double[size.Width];
+
+            // Divide the total range into a number of points equal to the width
+            double total = (max - min) / size.Width;
+
+            // initializing x,y values for the sin function
+            for (int i = 0; i < x.Length; i++)
+            {
+                x[i] = min + (total * i);
+                y[i] = Math.Sin(x[i]);
+            }
+
+            graphFunction(x, y, g);
+        }
+
+        void graphFunction(double[] x, double[] y, Graphics g)
+        {
+            double minY = minValue(y);
+            double maxY = maxValue(y);
+
+            double[] px = new double[x.Length];
+            double[] py = new double[y.Length];
+
+            Size size = r.Size;
+
+            for (int ip = 0; ip < x.Length; ip++)
+            {
+                px[ip] = 20 + (x[ip] - min) / (max - min) * size.Width;
+                py[ip] = 100 + (size.Height - ((y[ip] - minY) / (maxY - minY) * size.Height));
+            }
+
+            Pen bluePen = new Pen(Color.Blue);
+
+            for (int ip = 1; ip < x.Length; ip+=2)
+            {
+                g.DrawLine(bluePen, (float)px[ip - 1], (float)py[ip - 1], (float)px[ip], (float)py[ip]);
+            }
+        }
+
+        double maxValue(double[] x)
+        {
+            double max = x[0];
+            foreach (double a in x)
+            {
+                if (max < a) max = a;
+            }
+            return max;
+        }
+
+        double minValue(double[] x)
+        {
+            double min = x[0];
+            foreach (double a in x)
+            {
+                if (min > a) min = a;
+            }
+            return min;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+
+        private void startValue_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                paint();
+            }
+        }
+
+    }
+}
