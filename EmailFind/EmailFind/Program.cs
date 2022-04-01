a35fd06e54e6a0b1aff21342ccd85e22e3ecc5ed
Russell <90806285+ArceyXD@users.noreply.github.com>
2022-03-31 23:18:57 -0400

    Add files via upload

---
+++ EmailFind/EmailFind/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.IO;
+namespace EmailFind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"mailto:(\w+@\w+\.\w{3})";
+            Regex rex = new Regex(pattern);
+
+            //step through the websites for phone info
+            WebClient client = new WebClient();
+            //clear file if existing 
+            File.Delete(@"C:\Users\Russell\source\repos\EmailFind\EmailFind\Emails.txt");
+            for (char c = 'A'; c<= 'Z'; c++)
+            {
+                string url = "https://web.ramapo.edu/phone/?type=" + c;
+                string rcInfo = client.DownloadString(url);
+                //Console.WriteLine(rcInfo.Length);
+
+                MatchCollection matches = rex.Matches(rcInfo);
+               
+                File.AppendAllText(@"C:\Users\Russell\source\repos\EmailFind\EmailFind\Emails.txt", "Last Name: " + c + Environment.NewLine + Environment.NewLine);
+                foreach (Match m in matches)
+                {
+                    //Console.WriteLine(m.Groups[1].Value);
+                    File.AppendAllText(@"C:\Users\Russell\source\repos\EmailFind\EmailFind\Emails.txt", m.Groups[1].Value + Environment.NewLine);
+                }
+
+                File.AppendAllText(@"C:\Users\Russell\source\repos\EmailFind\EmailFind\Emails.txt", Environment.NewLine);
+            }
+
+          
+        }
+
+
+        /*void getMatches(Regex reg, char index)
+        {
+
+
+
+        }
+        */
+    }
+}
---
+++ Quiz4/Quiz4/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            float[] y = new float[7];
+            y[0] = 20;
+            y[1] = 40;
+            y[2] = 60;
+            y[3] = 30;
+            y[4] = 10;
+            y[5] = 50;
+            y[6] = 100;
+
+            Size size = this.ClientSize;
+
+            float maxY = MaxValue(y);
+
+            float bh = size.Height / y.Length;
+
+
+            float[] bw = new float[y.Length];
+
+            for (int i = 0; i < y.Length; i++)
+            {
+                bw[i] = size.Width * y[i] / maxY;
+            }
+
+            //Draw bars
+
+            g.Clear(Color.White);
+
+            SolidBrush redBrush = new SolidBrush(Color.Red);
+            SolidBrush blueBrush = new SolidBrush(Color.Blue);
+            SolidBrush greenBrush = new SolidBrush(Color.Green);
+
+
+            for (int i = 0; i < y.Length; i++)
+            {
+                float xv = 0;
+                float yv = i * bh;
+
+                if (i % 3 == 0)
+                {
+                    g.FillRectangle(redBrush, xv, yv, bw[i], bh);
+                }
+
+                if (i % 3 == 1)
+                {
+                    g.FillRectangle(blueBrush, xv, yv, bw[i], bh);
+                }
+
+                if (i % 3 == 2)
+                {
+                    g.FillRectangle(greenBrush, xv, yv, bw[i], bh);
+                }
+
+
+            }
+        }
+
+
+        private float MaxValue(float[] x)
+        {
+            float max = x[0];
+            foreach (float a in x) if (max < a) max = a;
+
+            return max;
+        }
+    }
+}
